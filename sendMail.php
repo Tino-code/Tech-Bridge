4679a07ac48f92d5d1c9caff256b1f04f1795cf7
Tino-code <martintetteh1350@gmail.com>
2024-05-22 18:27:16 +0000

    updated contact and disabled donation

--- sendMail.php
+++ sendMail.php
@@ -1,14 +1,38 @@
 <?php
-
-$name = $_POST["name"];
-$email = $_POST["email"];
-$message = $_POST["Message"];
-$subject="Message from Webmaster";
 require "vendor/autoload.php";
-
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
+$errors = array(
+    'name' => '',
+    'email' => '',
+    
+);
+if (isset($_POST["submit"])){
 
+
+    $name = trim($_POST['name']);
+    $emails = trim($_POST['email']);
+
+    if (empty($name) || !preg_match('/^[a-zA-Z\s\-]{5,}$/', $name)) {
+        $errors['name'] = "Please enter a valid full name with no numbers (at least 5 characters)";
+    }
+
+
+   
+    if (empty($emails) || !filter_var($emails, FILTER_VALIDATE_EMAIL)) {
+        $errors['email'] = "Please enter a valid email address";
+    }
+
+
+
+
+
+if (!array_filter($errors)) {
+
+    $name = $_POST["name"];
+$emails = $_POST["email"];
+$message = $_POST["Message"];
+$subject="An email has been sent from ".$emails;
 $mail = new PHPMailer(true);
 
 // $mail->SMTPDebug = SMTP::DEBUG_SERVER;
@@ -23,7 +47,7 @@ $mail->Port = 587;
 $mail->Username = "martintetteh1350@gmail.com";
 $mail->Password = "mrsz jhzh pgjn bfzg";
 
-$mail->setFrom($email, $name);
+$mail->setFrom($emails, $name);
 $mail->addAddress("tettehmartin217@gmail.com","Martin Tetteh");
 
 $mail->Subject = $subject;
@@ -31,4 +55,7 @@ $mail->Body = $message;
 
 $mail->send();
 
-header("Location: contact.html");
+  echo $emails;
+ }
+    
+}
